05f1fdea79c3b159de4d4a1b2bd51d2955a4c534
nativedone <adeildoamorim@gmail.com>
2023-05-01 12:00:21 -0300

    controls canvas height

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { App as Canvas } from './Canvas'
 import Overlay from './Overlay'
+import { ContentSection } from './ContentSection'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Canvas />
+    {/* Controls canvas height  */}
+    <div style={{ height: '120%' }}>
+      <Canvas />
+    </div>
     <Overlay />
+    <ContentSection title="Section 2"/>
+    <ContentSection title="Section 3"/>
+    <ContentSection title="Section 4"/>
   </>
 )
